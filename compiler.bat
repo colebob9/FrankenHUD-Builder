abc56f9916251d6e80480fb5994fd0bdf2aa2267
colebob9 <colebob9@gmail.com>
2015-12-26 21:41:22 -0600

    started work on file organizing

--- compiler.bat
+++ compiler.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+:: File Check
 ECHO Checking for required files...
 IF NOT EXIST wget.exe ECHO wget.exe could not be found! Please download a new release to fix this. & GOTO filewarning
 IF NOT EXIST 7za.exe ECHO 7za.exe could not be found! Please download a new release to fix this. & GOTO filewarning
@@ -14,6 +15,7 @@ ECHO.
 PAUSE
 EXIT
 :filesokay
+:: Title
 CLS
 ECHO FrankenHUD Builder vX.X.X
 ECHO Written by colebob9
@@ -23,15 +25,28 @@ ECHO.
 ECHO This will compile the TF2 HUD FrankenHUD from the GitHub repo automatically.
 ECHO You must have TF2 installed to compile.
 PAUSE
+:: Download and Extract
 wget.exe -O master.zip https://github.com/TheKins/frankenhud/archive/master.zip --no-check-certificate
 7za.exe x *.zip
+:: Make build directories
 md build
 md build\frankenhud-core
 md build\frankenhud-style
 md build\frankenhud-fonts
+:: Organizing files in right order
+XCOPY /E /I "frankenhud-master\hud_src\resource" "build\frankenhud-core\resource"
+XCOPY /E /I "frankenhud-master\hud_src\scripts" "build\frankenhud-core\scripts"
+MOVE "build\frankenhud-core\resource\monofont.tff" "build\frankenhud-fonts\"
+MOVE "build\frankenhud-core\resource\tf2modified.tff" "build\frankenhud-fonts\"
+:: VPK Packer
+"%ProgramFiles(x86)%\Steam\steamapps\common\Team Fortress 2\bin\vpk.exe" build\frankenhud-core
+:: Clean up
+ECHO.
+Choice /M "Clean up files?"
+:: No
+If Errorlevel 2 GOTO afterclean
+:: Yes
+If Errorlevel 1 GOTO clean
+:clean
 
-XCOPY "frankenhud-master\hud_src\resource" "build\frankenhud-core\resource"
-
-pause
-"%ProgramFiles(x86)%\Steam\steamapps\common\Team Fortress 2\bin\vpk.exe" frankenhud-master
-pause
+:afterclean
