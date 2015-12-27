9c04cbb3510ec6c7a253f10ff0b7a4ba5d383814
colebob9 <colebob9@gmail.com>
2015-12-26 20:54:48 -0600

    now checks for required files

--- compiler.bat
+++ compiler.bat
@@ -1,4 +1,19 @@
 @ECHO OFF
+ECHO Checking for required files...
+IF NOT EXIST wget.exe ECHO wget.exe could not be found! Please download a new release to fix this. & GOTO filewarning
+IF NOT EXIST 7za.exe ECHO 7za.exe could not be found! Please download a new release to fix this. & GOTO filewarning
+IF NOT EXIST libeay32.dll ECHO libeay32.dll could not be found! Please download a new release to fix this. & GOTO filewarning
+IF NOT EXIST libiconv2.dll ECHO libiconv2.dll could not be found! Please download a new release to fix this. & GOTO filewarning
+IF NOT EXIST libintl3.dll ECHO libintl3.dll could not be found! Please download a new release to fix this. & GOTO filewarning
+IF NOT EXIST libssl32.dll ECHO libssl32.dll could not be found! Please download a new release to fix this. & GOTO filewarning
+ECHO All files found! Continuing. 
+GOTO filesokay
+:filewarning
+ECHO.
+PAUSE
+EXIT
+:filesokay
+CLS
 ECHO FrankenHUD Builder vX.X.X
 ECHO Written by colebob9
 ECHO Released under the MIT licence.
@@ -9,5 +24,5 @@ ECHO You must have TF2 installed to compile.
 PAUSE
 wget.exe -O master.zip https://github.com/TheKins/frankenhud/archive/master.zip --no-check-certificate
 7za.exe x *.zip
-"%ProgramFiles(x86)%\Steam\steamapps\common\Team Fortress 2\bin\vpk.exe"
+"%ProgramFiles(x86)%\Steam\steamapps\common\Team Fortress 2\bin\vpk.exe" frankenhud-master
 pause
